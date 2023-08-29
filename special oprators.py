423b9b137836638b5c977c9dcabb392cdb036464
Vaibhav Mahadeo Raut <123192741+Vaibhavraut015@users.noreply.github.com>
2023-08-29 16:08:23 +0530

    Add files via upload
    29/8/2023

---
+++ special oprators.py
@@ -0,0 +1,21 @@
+#identity oprators
+#it includes is and is not etc. it gives answer as true or false
+
+a=30
+b=30
+print(a is b)
+
+a=39
+b=29
+print(a is not b)
+
+# membership oprator
+#it includes in and not in etc.
+
+a = [1,3,4,6,45,2,23,4,2]
+b=7
+print(b in a)
+
+a = [1,3,4,6,45,2,23,4,2]
+b=7
+print(b not in a)
